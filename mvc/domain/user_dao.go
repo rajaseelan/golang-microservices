910b2e00160510cb2f8da22ac59806525f485c2c
Rajaseelan Ganeswaran <rajaseelan@users.noreply.github.com>
2020-08-15 16:15:06 +0100

    benchmarks (#2)

--- mvc/domain/user_dao.go
+++ mvc/domain/user_dao.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	users = map[int64]*User{
-		123: {Id: 1, FirstName: "Fede", LastName: "Leon", Email: "f@w.com"},
+		123: {Id: 123, FirstName: "Fede", LastName: "Leon", Email: "f@w.com"},
 	}
 )
 
---
+++ mvc/utils/sort_utils.go
@@ -0,0 +1,17 @@
+package utils
+
+// []int{9,8,7,6,5}
+func BubbleSort(elements []int) []int {
+	keepRunning := true
+	for keepRunning {
+		keepRunning = false
+
+		for i := 0; i < len(elements)-1; i++ {
+			if elements[i] > elements[i+1] {
+				elements[i], elements[i+1] = elements[i+1], elements[i]
+				keepRunning = true
+			}
+		}
+	}
+	return elements
+}
